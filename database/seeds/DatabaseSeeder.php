1d58dfdaefc812e838acd226b0521d3c523510c7
FaizFizy Rosle <faizfizy@gmail.com>
2016-04-19 11:36:28 +0800

    Call all seeds in DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,30 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run() {
+        $this->call(UsersTableSeeder::class);
+        $this->call(DevicesTableSeeder::class);
+        $this->call(VerificationsTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
+        $this->call(ProductImagesTableSeeder::class);
+        $this->call(FollowsTableSeeder::class);
+        $this->call(LikesTableSeeder::class);
+        $this->call(SubCategoriesTableSeeder::class);
+        $this->call(CartsTableSeeder::class);
+        $this->call(OrdersTableSeeder::class);
+        $this->call(MessagesTableSeeder::class);
+        $this->call(NotificationsTableSeeder::class);
+        $this->call(PaymentDetailsTableSeeder::class);
+        $this->call(PaymentsTableSeeder::class);
+        $this->call(ReviewsTableSeeder::class);
     }
+
 }
