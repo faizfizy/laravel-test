1a9dcceef3335bc077cb81f6278d6b3529ca050a
FaizFizy Rosle <faizfizy@gmail.com>
2016-04-20 10:45:21 +0800

    Basic JSON resource endpoint

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,37 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register all of the routes for an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('users', 'UserController', ['only' => [
+        'index', 'show'
+]]);
+Route::resource('categories', 'CategoryController', ['only' => [
+        'index', 'show'
+]]);
+Route::resource('products', 'ProductController', ['only' => [
+        'index', 'show'
+]]);
+Route::resource('carts', 'CartController', ['only' => [
+        'index', 'show'
+]]);
+Route::resource('likes', 'LikeController', ['only' => [
+        'index', 'show'
+]]);
+Route::resource('notifications', 'NotificationController', ['only' => [
+        'index', 'show'
+]]);
+Route::resource('messages', 'MessageController', ['only' => [
+        'index', 'show'
+]]);
